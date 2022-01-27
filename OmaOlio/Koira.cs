d394459de3cbfd07cc16be771957772450698163
Jesse Nisonen <jesse.nisonen@student.careeria.fi>
2022-01-27 13:42:43 +0200

    OmaOlioTehtävä

---
+++ OmaOlio/Koira.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmaOlio
+{
+    internal class Koira
+    {
+        public string Nimi { get; set; }
+        public string Rotu { get; set; }
+        public int Ikä { get; set; }
+        public String Väri { get; set; }
+
+        public float Paino { get; set; }
+
+        
+         
+        
+
+        public string HaeTiedot()
+        {
+            string KoiranTiedot = "Nimi: " + Nimi + ". Rotu: " + Rotu + ". Ikä: " + Ikä.ToString() + ". Väri: " + Väri + ". Paino: " + Paino.ToString();
+            return KoiranTiedot;
+
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine("Woof Woof");
+
+
+        }
+        
+        public void Temppu()
+        {
+            Console.WriteLine("360");
+        }
+
+        
+
+        
+    }
+}
---
+++ OmaOlio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OmaOlio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Koira KoiraOlio = new Koira();
+
+            KoiraOlio.Nimi = "Rekku";
+            KoiraOlio.Rotu = "Saksanpaimenkoira";
+            KoiraOlio.Ikä = 4;
+            KoiraOlio.Väri = "Harmaa";
+            KoiraOlio.Paino =30.0f;
+
+            KoiraOlio.Hauku();
+            
+            Console.WriteLine(KoiraOlio.HaeTiedot());
+
+            KoiraOlio.Hauku();
+            }
+
+            
+       
+
+    }
+}
