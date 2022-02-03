0e796e4b2b2481f4d81991f634682104c1815034
Jesse Nisonen <jesse.nisonen@student.careeria.fi>
2022-02-03 12:17:25 +0200

    Harjoitus3

---
+++ Kiuas/Kiuas.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kiuas
+{
+    internal class Kiuas
+    {
+        private string Nimi { get; set; }
+        private bool Tila { get; set; }
+
+        private int Lämpötila { get; set; }
+
+        private int Kosteus { get; set; }
+
+        public Kiuas(string _nimi)
+
+        {
+            Nimi = _nimi;
+
+            Tila = false;
+            Lämpötila = 10;
+            Kosteus = 80;
+
+        }
+
+        public void TulostaData()
+
+        {
+            Console.WriteLine("Kiuas: ");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Tila: " + NäytäTila());
+            Console.WriteLine("-- Lämpötila: " + Lämpötila + " Celsius");
+            Console.WriteLine("-- Kosteus: " + Kosteus + "%");
+            Console.WriteLine();  
+
+        }
+        public string NäytäTila()
+        {
+            string nykyinenTila = "Pois Päältä";
+
+            if (Tila == true)
+            {
+                nykyinenTila = "Päällä";
+            }
+
+            return nykyinenTila;
+        }
+        public void KiuasOnOff(bool t)
+        {
+            Tila = t;
+
+            Console.WriteLine("Kiuas on nyt " + NäytäTila() + "\n");
+
+        }
+        public void MuutaLämpötilaa(int uusilämpötila)
+        { 
+               if (Tila == false)
+            {
+                Console.WriteLine("Kiuas ei ole päällä, lämpötilaa ei voida muuttaa\n");
+                return; // jos kiuas ei ole päällä, poistutaan metodista
+            }
+
+            Lämpötila = uusilämpötila;
+
+            if (Lämpötila < 10)
+            {
+                Lämpötila = 10;
+            }
+            else if (Lämpötila > 150)
+            {
+                Lämpötila = 150;
+            }
+
+            Console.WriteLine("Lämpötila muutettu. Uusi lämpötila: " + Lämpötila + " celsius\n");
+
+        }
+        public void Muutakosteutta(int UusiKosteus)
+        {
+            if (Tila == false)
+            {
+                Console.WriteLine("Kiuas ei ole päällä, kosteutta ei voida muuttaa\n");
+                return; // jos kiuas ei ole päällä, poistutaan metodista 
+
+            }
+
+            Kosteus = UusiKosteus;
+
+            if (Kosteus < 0)
+            {
+                Kosteus = 0;
+            }
+            else if (Kosteus > 100)
+            {
+                Kosteus = 100;
+            }
+            Console.WriteLine("Kosteutta muutettu. Uusi kosteus: " + Kosteus + "%\n");
+        }
+    }
+    
+        
+}
---
+++ Kiuas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kiuas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas = new Kiuas("Harvia");
+
+            kiuas.TulostaData();
+
+            kiuas.MuutaLämpötilaa(70);
+
+            kiuas.KiuasOnOff(true);
+            kiuas.MuutaLämpötilaa(70);
+            kiuas.Muutakosteutta(90);
+
+            kiuas.TulostaData();
+
+            kiuas.KiuasOnOff(false);
+        }
+    }
+}
